47375487dc1ffdd9a2fc2f955f4f8f15f28f8707
Chad Bearden <chadb@beardendesigns.com>
2008-11-25 11:27:48 -0500

    added autotest to link list

--- link.sh
+++ link.sh
@@ -13,11 +13,7 @@ mv -f .signature $OLD
 mv -f .vim $OLD
 mv -f .vimrc $OLD
 mv -f .gitconfig $OLD
-#mv -f .subversion $OLD
-#mv -f .svn $OLD
-#mv -f .cvsignore $OLD
-#mv -f .bash_prompt $OLD
-#mv -f .bash_login $OLD
+mv -f .autotest $OLD
 
 ln -s $DOTFILES/.profile .profile
 ln -s $DOTFILES/.bashrc .bashrc
@@ -28,8 +24,4 @@ ln -s $DOTFILES/.signature .signature
 ln -s $DOTFILES/.gitconfig .gitconfig
 ln -s $DOTFILES/.vim .vim
 ln -s $DOTFILES/.vimrc .vimrc
-#ln -s $DOTFILES/.svn .svn
-#ln -s $DOTFILES/.subversion .subversion
-#ln -s $DOTFILES/.bash_login .bash_login
-#ln -s $DOTFILES/.bash_prompt .bash_prompt
-#ln -s $DOTFILES/.cvsignore .cvsignore
+ln -s $DOTFILES/.autotest .autotest
