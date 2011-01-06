e38ac2829f21dd0197ba9b53f6980acf5afde385
Chad Bearden <chadb@beardendesigns.com>
2011-01-06 09:17:28 -0500

    added screenrc

--- link.sh
+++ link.sh
@@ -4,7 +4,7 @@ OLD=${DOTFILES}/backup/`date  +%Y%m%d%H%M%S`
 cd $HOME
 mkdir -p $OLD
 
-files=( .bash_profile .bashrc .bash_logout .ctags .gvimrc .signature .vim .vimrc .gitconfig .autotest .irbrc .dir_colors .gemrc )
+files=( .bash_profile .bashrc .bash_logout .ctags .gvimrc .signature .vim .vimrc .gitconfig .autotest .irbrc .dir_colors .gemrc .screenrc)
 
 for file in ${files[*]}
 do
